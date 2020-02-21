e9f500559a75eb206561ccb82356be959a5b17a1
oliverlogie <oliver.s.kraus@gmail.com>
2020-02-21 17:06:09 +0100

    added style

--- js/script.js
+++ js/script.js
@@ -1,23 +1,13 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 var objPlaces;
 objPlaces = [];
 var iArray = [];
+var aArray = [];
 var theLocation = ["Vienna", "Stephansdom", "Schönbrunn", "Jamaica"];
 var zipCode = ["1020", "1220", "1130", "1000"];
 var adress = ["Riesenradplatz 1, Vienna", "Stephansplatz 3, Vienna", "Maxingstraße 13b, 1130 Vienna", "blalal"];
 var imgPlace = ["img/prater.jpg", "img/Stephansdom.jpg", "img/Schonbrunn.jpg", "img/Schonbrunn.jpg"];
+var food = ["Burger", "Fish", "Fries", "Schnitzel"];
+var link = ["http://www.restaurant-on.at/", "biofrische.wien", "https://www.mjam.net/", "https://www.leontine.at/"];
 var Places = /** @class */ (function () {
     function Places(theLocation, zipCode, adress, imgPlace) {
         this.theLocation = theLocation;
@@ -37,7 +27,7 @@ function createCard() {
     var CardsLocation = "";
     for (var i = 0; i < objPlaces.length; i++) {
         iArray.push(i);
-        CardsLocation += "\n        <div class=\"card text-white p-0 col-lg-3 col-md-6 col-sm-12 bg-warning border\">\n                        <div  class=\"Card\">\n                        <p><img src=\"" + objPlaces[i].imgPlace + "\" class=\"imgLoc\" alt=\"" + objPlaces[i].theLocation + "\"></p>\n                        <h3>" + objPlaces[i].theLocation + "</h3>\n                        <div class=\"hideInfo\">\n                        <p>" + objPlaces[i].zipCode + "</p>\n                        <p>" + objPlaces[i].adress + "</p>\n                        </div>\n                        <button type=\"submit\" class=\"p-3\" id=\"button\">More Infos</button>\n                        </div>\n                    </div>\n                </div>\n            </div>\n        </div>\n        ";
+        CardsLocation += "\n        <div class=\"card m-0 text-black col-lg-2 col-md-6 col-sm-12 border\">\n                        <div  class=\"Card\">\n                        <p><img src=\"" + objPlaces[i].imgPlace + "\" class=\"imgLoc\" alt=\"" + objPlaces[i].theLocation + "\"></p>\n                        <h3>" + objPlaces[i].theLocation + "</h3>\n                        <div class=\"hideInfo\">\n                        <p>" + objPlaces[i].zipCode + "</p>\n                        <p>" + objPlaces[i].adress + "</p>\n                        </div>\n                        <button type=\"submit\" class=\"p-3\" id=\"button\">More Infos</button>\n                        </div>\n                    </div>\n                </div>\n            </div>\n        </div>\n        ";
     }
     ;
     document.getElementById('Loc').innerHTML = CardsLocation;
@@ -50,14 +40,45 @@ $(document).ready(function () {
         $(this).parent().find(".hideInfo").toggle();
     });
 });
-var Restaurant = /** @class */ (function (_super) {
-    __extends(Restaurant, _super);
-    function Restaurant(theLocation, zipCode, adress, imgPlace, food, link) {
-        var _this = _super.call(this, theLocation, zipCode, adress, imgPlace, food, link) || this;
-        _this.food = food;
-        _this.link = link;
-        return _this;
-    }
-    return Restaurant;
-}(Places));
-var food = new Restaurant("");
+// class Restaurant extends Places{
+//     food:string;
+//     link:string;
+//     constructor(theLocation, zipCode, adress, imgPlace, food, link){
+//         super(theLocation, zipCode, adress, imgPlace);
+//         this.food = food;
+//         this.link = link;
+//     }
+// }
+// function creationRestaurant(){
+//     for (let i = 0; i<4; i++){
+//         let placeRes = new Places(theLocation[i], zipCode[i],adress[i],imgPlace[i],food[i],link[i])
+//         objPlaces.push(placeRes);
+//     }
+// }
+// function createRestaurant(){
+//     var CardsRestaurant = "";
+//     for (let i=0; i < objPlaces.length; i++) {
+//         aArray.push(i);
+//         CardsRestaurant += `
+//         <div class="card text-white p-0 col-lg-3 col-md-6 col-sm-12 bg-warning border">
+//                         <div  class="Card">
+//                         <p><img src="${objPlaces[i].imgPlace}" class="imgLoc" alt="${objPlaces[i].theLocation}"></p>
+//                         <h3>${objPlaces[i].theLocation}</h3>
+//                         <div class="hideInfo">
+//                         <p>${objPlaces[i].zipCode}</p>
+//                         <p>${objPlaces[i].adress}</p>
+//                         </div>
+//                         <p>${objPlaces[i].food}</p>
+//                         <p>${objPlaces[i].link}</p>
+//                         <button type="submit" class="p-3" id="button">More Infos</button>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//         `;
+//    };
+//    document.getElementById('Loc').innerHTML = CardsRestaurant;
+// };
+// creationRestaurant();
+// createRestaurant();
--- js/script.ts
+++ js/script.ts
@@ -1,11 +1,14 @@
 let objPlaces: Array<Places>;
 objPlaces = [];
 let iArray = [];
+let aArray = [];
 
 let theLocation: Array<string>= ["Vienna", "Stephansdom","Schönbrunn","Jamaica"];
 let zipCode: Array<string>= ["1020","1220", "1130", "1000"];
 let adress:	Array<string>= ["Riesenradplatz 1, Vienna", "Stephansplatz 3, Vienna", "Maxingstraße 13b, 1130 Vienna","blalal"];
 let imgPlace: Array<string>= ["img/prater.jpg","img/Stephansdom.jpg", "img/Schonbrunn.jpg","img/Schonbrunn.jpg"];
+let food: Array<string>= ["Burger", "Fish", "Fries", "Schnitzel"];
+let link: Array<string>=["http://www.restaurant-on.at/","biofrische.wien","https://www.mjam.net/","https://www.leontine.at/"];
 
 class Places {
 	theLocation: string;
@@ -33,7 +36,7 @@ function createCard(){
     for (let i=0; i < objPlaces.length; i++) {
         iArray.push(i);
         CardsLocation += `
-        <div class="card text-white p-0 col-lg-3 col-md-6 col-sm-12 bg-warning border">
+        <div class="card m-0 text-black col-lg-2 col-md-6 col-sm-12 border">
                         <div  class="Card">
                         <p><img src="${objPlaces[i].imgPlace}" class="imgLoc" alt="${objPlaces[i].theLocation}"></p>
                         <h3>${objPlaces[i].theLocation}</h3>
@@ -61,15 +64,49 @@ $(document).ready(function(){
     })
 })
 
-class Restaurant extends Places{
-    food:string;
-    link:string;
+// class Restaurant extends Places{
+//     food:string;
+//     link:string;
 
-    constructor(theLocation, zipCode, adress, imgPlace, food, link){
-        super(theLocation, zipCode, adress, imgPlace, food, link);
-        this.food = food;
-        this.link = link;
-    }
-}
+//     constructor(theLocation, zipCode, adress, imgPlace, food, link){
+//         super(theLocation, zipCode, adress, imgPlace);
+//         this.food = food;
+//         this.link = link;
+//     }
+// }
 
-let food = new Restaurant("")
+// function creationRestaurant(){
+//     for (let i = 0; i<4; i++){
+//         let placeRes = new Places(theLocation[i], zipCode[i],adress[i],imgPlace[i],food[i],link[i])
+//         objPlaces.push(placeRes);
+//     }
+// }
+
+// function createRestaurant(){
+//     var CardsRestaurant = "";
+//     for (let i=0; i < objPlaces.length; i++) {
+//         aArray.push(i);
+//         CardsRestaurant += `
+//         <div class="card text-white p-0 col-lg-3 col-md-6 col-sm-12 bg-warning border">
+//                         <div  class="Card">
+//                         <p><img src="${objPlaces[i].imgPlace}" class="imgLoc" alt="${objPlaces[i].theLocation}"></p>
+//                         <h3>${objPlaces[i].theLocation}</h3>
+//                         <div class="hideInfo">
+//                         <p>${objPlaces[i].zipCode}</p>
+//                         <p>${objPlaces[i].adress}</p>
+//                         </div>
+//                         <p>${objPlaces[i].food}</p>
+//                         <p>${objPlaces[i].link}</p>
+//                         <button type="submit" class="p-3" id="button">More Infos</button>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//         `;
+//    };
+//    document.getElementById('Loc').innerHTML = CardsRestaurant;
+// };
+// creationRestaurant();
+
+// createRestaurant();
